6630f0219b749cc959bac2b55f87fe870c1f10b3
Mircea <mircea@code11.com>
2019-10-02 17:22:38 +0300

    added dummy data

---
+++ patients.js
@@ -0,0 +1,55 @@
+var patientsList = [
+	{
+		id: 0,
+		name: 'Erik Torbjorn',
+		desc: 'My grandfather is missing an ear. Melanoma was eating away the left one, so it had to be removed. He’s hobbled by bad legs, particularly the right one, banged-up since college—two members of a forgotten Big Ten special teams unit both decided to tackle his kneecap at the same time. His feet swell at night for unknown reasons, but soaking them in ice water helps. Walking is difficult; he relies on a cane, or two hands along the wall.  ',
+		age: 80,
+		picture: '../../demo/erik/avatar.jpg',
+		birthdate: '03/03/1937',
+		birthplace: 'Olbu',
+		about: 'Lorem ipsum dolor molestie in volutpat eget, finibus vitae leo. Quisque ac ante nec odio bibendum euismod at quis leo. Duis blandit velit eget lobortis rhoncus. Mauris viverra ac lacus quis sagittis. ',
+		interests: 'Quisque ac ante nec odio bibendum euismod',
+		albumFavorites: [
+			{
+				thumb: '\demo\erik\thumb\erik_2.jpg',
+				thumb: '\demo\erik\thumb\erik_3.jpg',
+				thumb: '\demo\erik\thumb\erik_6.jpg'
+			}
+		],
+		relatives: [
+			id: 10,
+			name: 'Alex Torbjorn',
+			picture: '../../demo/erik/alex.jpg',
+			relation: 'son',
+			about: 'Lorem ipsum dolor molestie in volutpat eget, finibus vitae leo. Quisque ac ante nec odio bibendum euismod at quis leo.'
+		],
+		relatives: [
+			id: 11,
+			name: 'Joanna Torbjorn',
+			picture: '../../demo/erik/joanna.jpg',
+			relation: 'niece',
+			about: 'Lorem ipsum dolor molestie in volutpat eget, finibus vitae leo. Quisque ac ante nec odio bibendum euismod at quis leo.'
+		]
+	},
+	{
+		id: 1,
+		name: 'Andreea Torbjorn',
+		desc: 'It was over 100 years old but everyone loved that place',
+		age: 80,
+		picture: '../../demo/andreea/avatar.jpg',
+		birthdate: '03/03/1937',
+		birthplace: 'Olbu',
+		about: 'Lorem ipsum dolor molestie in volutpat eget, finibus vitae leo. Quisque ac ante nec odio bibendum euismod at quis leo. Duis blandit velit eget lobortis rhoncus. Mauris viverra ac lacus quis sagittis. ',
+		interests: 'Quisque ac ante nec odio bibendum euismod',
+		albumFavorites: [
+			{
+				thumb: '\demo\erik\thumb\erik_2.jpg',
+				thumb: '\demo\erik\thumb\erik_3.jpg',
+				thumb: '\demo\erik\thumb\erik_6.jpg'
+			}
+		],
+	}
+];
+
+
+module.exports = patientsList;
